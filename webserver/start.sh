ee990413851f24a5e33486dc26631dfa25402730
Ubuntu <ubuntu@ip-172-31-35-195.us-west-2.compute.internal>
2016-05-07 20:17:32 +0000

    move gunicorn to local only

--- webserver/start.sh
+++ webserver/start.sh
@@ -1,8 +1,13 @@
+#!/bin/sh
+
+
 # To start the server:
 # NB: gunicorn is designed to run behing a reverse proxy like nginx,
 # so this is a bit unsafe 
 gunicorn \
    app \
-   -b 0.0.0.0:8000 \
-   -D
+   -b 127.0.0.1:8000 \
 
+
+# now start nginx
+nginx 
